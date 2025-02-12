32d690bcd6a0ba135526ca145468d31a89658b2d
Anik Islam <anik54908@gmail.com>
2025-02-12 22:47:54 +0600

    just create a simple file structure of react-knowledge-cafe oroject

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+
+function App() { 
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-green-500 text-white py-2 px-8'>Vite + React</h1> 
+    </>
+  )
+}
+
+export default App
